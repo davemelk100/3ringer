ad3de47f20b76905657df3eff5bbec1b2cadc7c4
davemelk100 <davemelk@gmail.com>
2024-12-09 15:37:56 -0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,10 +38,10 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
-        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="icon" href="/favicon.svg" sizes="any" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="16x16" />
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.svg+xml" />
         <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body className={`${roboto.variable} font-sans antialiased`}>
