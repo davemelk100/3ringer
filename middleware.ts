5a354bf3e7ab0c9bf85502f84619672dbdac22cb
davemelk100 <davemelk@gmail.com>
2024-12-15 22:52:12 -0500

    netlify fix

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,6 @@
-export const config = {
-  matcher: []
-};
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+export function middleware(request: NextRequest) {
+  return NextResponse.next()
+}
