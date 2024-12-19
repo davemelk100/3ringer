eb590a5decc65f8d9e57cc7aa30b14eb9a8db7f1
davemelk100 <davemelk@gmail.com>
2024-12-19 15:06:44 -0500

    cors updates

--- middleware.ts
+++ middleware.ts
@@ -1,3 +1,18 @@
 export const config = {
-  matcher: []
+  matcher: ["/api/:path*"],
 };
+
+export default function middleware() {
+  const res = new Response();
+
+  // Set CORS headers
+  res.headers.set("Access-Control-Allow-Credentials", "true");
+  res.headers.set("Access-Control-Allow-Origin", "*"); // Replace * with your domain in production
+  res.headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  return res;
+}
