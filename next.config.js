b9f89ef165dd53f99eea41a3f85e75b9181d6029
davemelk100 <davemelk@gmail.com>
2024-12-28 20:39:13 -0500

    trying to get rid of punycode error but it didn't but whatever

--- next.config.js
+++ next.config.js
@@ -1,9 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@auth0/auth0-react"],
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false, net: false, tls: false };
+  webpack: (config, { isServer }) => {
+    // Suppress warnings
+    config.ignoreWarnings = [
+      { module: /node_modules\/node-fetch\/lib\/index\.js/ },
+      { module: /node_modules\/punycode\/punycode\.js/ },
+    ];
+
+    // Add fallbacks for node modules
+    config.resolve.fallback = {
+      fs: false,
+      net: false,
+      tls: false,
+      punycode: false,
+    };
+
     return config;
+  },
+  // Suppress specific build warnings
+  onDemandEntries: {
+    // Reduce log noise
+    maxInactiveAge: 25 * 1000,
+    pagesBufferLength: 2,
   },
 };
 
