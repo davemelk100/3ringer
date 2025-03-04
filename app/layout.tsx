974aed3d0112bc2767024d295e0cc3864fba4cd3
davemelk100 <davemelk@gmail.com>
2025-03-04 10:00:55 -0500

    revert

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({
-  weight: ["400", "500", "600", "700", "800", "900"],
+  weight: ["400", "500", "600", "700"],
   subsets: ["latin"],
 });
 
